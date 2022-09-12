88a46d5279857d891c246f16cf2f02a1d8e4ac09
KakashiJiyayou <rafibapi@gmail.com>
2022-09-12 18:34:28 +0800

    upload test.php

--- app/controller/Test.php
+++ app/controller/Test.php
@@ -28,7 +28,8 @@ class Test
 
     public function banner(){
         // return response(file_get_contents(dirname(dirname(__FILE__)).'/uploads/json/home/banner1.json'));
-        return response(file_get_contents(dirname(dirname(__FILE__)).'/view/banner1.json'));
+        return response(file_get_contents(dirname(dirname(__FILE__)).'/view/banner1.json')); 
+        
         // return response(file_get_contents(dirname(dirname(__FILE__)).'/view/index.html'));
     }
 }
