dc95cd56769df53faa5d978e6f3c0a7e38a23424
KakashiJiyayou <rafibapi@gmail.com>
2022-09-18 21:24:51 +0800

    Update name

---
+++ app/controller/Getid.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controller;
+
+use Error;
+use Exception;
+use app\model\Counters;
+use think\response\Html;
+use think\response\Json;
+use think\facade\Log;
+ 
+class Getid {
+
+    public function get_code($code){
+        $head = getallheaders();
+        // $body = json_decode(file_get_contents('php://input'),true);
+        $openid = !empty($head['x-wx-openid']) ? $head['x-wx-openid'] : $head['X-WX-OPENID'];
+
+        echo $openid;
+    }
+}
