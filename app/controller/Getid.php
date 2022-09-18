ca9262ae4337cad50ccc04e4e4d8b6198d0bc3c1
KakashiJiyayou <rafibapi@gmail.com>
2022-09-18 21:29:10 +0800

    Update

--- app/controller/Getid.php
+++ app/controller/Getid.php
@@ -11,7 +11,7 @@ use think\facade\Log;
  
 class Getid {
 
-    public function get_code($code){
+    public function get_code(){
         $head = getallheaders();
         // $body = json_decode(file_get_contents('php://input'),true);
         $openid = !empty($head['x-wx-openid']) ? $head['x-wx-openid'] : $head['X-WX-OPENID'];
