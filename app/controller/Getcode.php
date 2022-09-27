1c5e19f43cb62809201e2360f947b94865e61e40
KakashiJiyayou <rafibapi@gmail.com>
2022-09-28 00:16:13 +0800

    fixing opendata

--- app/controller/Getcode.php
+++ app/controller/Getcode.php
@@ -42,7 +42,7 @@ class Getcode {
     public function get_code(){
 		$head = getallheaders();
         $openid = !empty($head['x-wx-openid']) ? $head['x-wx-openid'] : $head['X-WX-OPENID'];
-
+		return $openid;
 		// return $openid;
         // $appId = "wxa6d55e3e5cb2a024";
         // $secret = "a3ad5bb4723c70dc66ecbc33e38325d7";
@@ -57,8 +57,8 @@ class Getcode {
 		// $data = json_decode($res,true);
 		// echo $data;
 
-		$get_opendata	= $this->get_opendata($openid);
-		return $get_opendata;
+		// $get_opendata	= $this->get_opendata($openid);
+		// return $get_opendata;
 
 
     }
