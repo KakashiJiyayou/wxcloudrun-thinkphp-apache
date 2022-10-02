3c87baeb8970dbeca5777a654574f26d79c789a3
KakashiJiyayou <rafibapi@gmail.com>
2022-10-02 13:43:19 +0800

    nothing

--- route/app.php
+++ route/app.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-use think\facade\Route;
+use think\facade\Route; 
 
 // 获取当前计数
 // Route::get('/api/count', 'index/getCount');
